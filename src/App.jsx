9c162ee39f0943b7a50698749ea8c493a8627b36
amayones <mayonedrive1@gmail.com>
2025-09-15 10:14:29 +0700

    initialize project with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./Routes";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
