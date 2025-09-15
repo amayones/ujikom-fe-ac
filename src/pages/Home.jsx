4920337c73fda7092fbdb9310fc184a168e2d98b
amayones <mayonedrive1@gmail.com>
2025-09-15 09:17:04 +0700

    initialize project with basic structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
         <div className="text-center py-20">
             <h1 className="text-4xl font-bold mb-4">Welcome to Absolute Cinema</h1>
             <p className="text-lg">Your ultimate destination for movies and entertainment.</p>
+            <p>edited</p>
         </div>
     )
 }
